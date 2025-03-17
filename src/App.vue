e01f82fd9913e5b9d7fce0dc6d3f37e8e4d94192
Juan Ramirez <juandiegoramirez071@gmail.com>
2025-03-17 16:28:51 -0500

    sideBar added

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,12 @@
 <script setup lang="ts">
 import NavBar from './shared/components/NavBar.vue';
+import SideBar from './shared/components/SideBar.vue';
 
 </script>
 
 <template>
   <NavBar />
-  
+  <SideBar />
 </template>
 
 <style scoped>
--- src/shared/components/SideBar.vue
+++ src/shared/components/SideBar.vue
@@ -1,84 +1,39 @@
 <template>
   <el-row class="tac">
-    <el-col :span="12">
-      <h5 class="mb-2">Default colors</h5>
+    <el-col :span="3">
       <el-menu
         default-active="2"
-        class="el-menu-vertical-demo"
+        class="el-menu-vertical-demo menu"
         @open="handleOpen"
         @close="handleClose"
       >
-        <el-sub-menu index="1">
-          <template #title>
-            <el-icon><location /></el-icon>
-            <span>Navigator One</span>
-          </template>
-          <el-menu-item-group title="Group One">
-            <el-menu-item index="1-1">item one</el-menu-item>
-            <el-menu-item index="1-2">item two</el-menu-item>
-          </el-menu-item-group>
-          <el-menu-item-group title="Group Two">
-            <el-menu-item index="1-3">item three</el-menu-item>
-          </el-menu-item-group>
-          <el-sub-menu index="1-4">
-            <template #title>item four</template>
-            <el-menu-item index="1-4-1">item one</el-menu-item>
-          </el-sub-menu>
-        </el-sub-menu>
-        <el-menu-item index="2">
-          <el-icon><icon-menu /></el-icon>
-          <span>Navigator Two</span>
-        </el-menu-item>
-        <el-menu-item index="3" disabled>
+        <el-menu-item index="1">
           <el-icon><document /></el-icon>
-          <span>Navigator Three</span>
+          <span>Llamadas</span>
         </el-menu-item>
-        <el-menu-item index="4">
-          <el-icon><setting /></el-icon>
-          <span>Navigator Four</span>
-        </el-menu-item>
-      </el-menu>
-    </el-col>
-    <el-col :span="12">
-      <h5 class="mb-2">Custom colors</h5>
-      <el-menu
-        active-text-color="#ffd04b"
-        background-color="#545c64"
-        class="el-menu-vertical-demo"
-        default-active="2"
-        text-color="#fff"
-        @open="handleOpen"
-        @close="handleClose"
-      >
-        <el-sub-menu index="1">
-          <template #title>
-            <el-icon><location /></el-icon>
-            <span>Navigator One</span>
-          </template>
-          <el-menu-item-group title="Group One">
-            <el-menu-item index="1-1">item one</el-menu-item>
-            <el-menu-item index="1-2">item two</el-menu-item>
-          </el-menu-item-group>
-          <el-menu-item-group title="Group Two">
-            <el-menu-item index="1-3">item three</el-menu-item>
-          </el-menu-item-group>
-          <el-sub-menu index="1-4">
-            <template #title>item four</template>
-            <el-menu-item index="1-4-1">item one</el-menu-item>
-          </el-sub-menu>
-        </el-sub-menu>
+
         <el-menu-item index="2">
-          <el-icon><icon-menu /></el-icon>
-          <span>Navigator Two</span>
-        </el-menu-item>
-        <el-menu-item index="3" disabled>
           <el-icon><document /></el-icon>
-          <span>Navigator Three</span>
+          <span>Clientes</span>
         </el-menu-item>
-        <el-menu-item index="4">
-          <el-icon><setting /></el-icon>
-          <span>Navigator Four</span>
+
+        <el-menu-item index="3">
+          <el-icon><document /></el-icon>
+          <span>Metricas</span>
         </el-menu-item>
+
+        <el-sub-menu index="4">
+          <template #title>
+            <el-icon><setting /></el-icon>
+            <span>Configuración</span>
+          </template>
+          <el-menu-item-group>
+            <el-menu-item index="4-1">Tarifas</el-menu-item>
+            <el-menu-item index="4-2">Bonos</el-menu-item>
+            <el-menu-item index="4-3">Permisos</el-menu-item>
+          </el-menu-item-group>
+        </el-sub-menu>
+
       </el-menu>
     </el-col>
   </el-row>
@@ -98,3 +53,12 @@ const handleClose = (key: string, keyPath: string[]) => {
   console.log(key, keyPath)
 }
 </script>
+
+<style scoped>
+
+.menu {
+  height: calc(100vh - 60px);
+  min-width: 200px;
+}
+
+</style>
